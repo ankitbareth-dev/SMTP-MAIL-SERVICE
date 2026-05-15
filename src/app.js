216d3e64cd0ecdbf1890c6dc7f947b4b9e7a6da3
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-05-15 21:24:41 +0530

    setup app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 import express from "express";
+import emailRoutes from "./routes/email.routes.js";
+import globalErrorHandler from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/email", emailRoutes);
+
+app.use(globalErrorHandler);
 
 export default app;
